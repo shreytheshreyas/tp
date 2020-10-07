a20e34d58011ad407ad8b62ae9712a2898dc2e85
Sam Chris <thesamchris@gmail.com>
2020-10-07 12:58:18 +0800

    Create team member functionality

---
+++ src/main/java/seedu/duke/TeamMember.java
@@ -0,0 +1,12 @@
+package seedu.duke;
+
+public class TeamMember {
+//    protected Integer id;
+    protected String name;
+
+    public TeamMember(String name) {
+        this.name = name;
+//        this.id = TeamMemberList.memberList.size();
+    }
+
+}
