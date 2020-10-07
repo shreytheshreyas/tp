df9b52fac7bf72cddb85041096d1047a71e246aa
shreytheshreyas <59922045+shreytheshreyas@users.noreply.github.com>
2020-10-07 19:13:12 +0800

    Update Project.java
    Resolved code style in this file and made symbols into constants

--- src/main/java/seedu/duke/Project.java
+++ src/main/java/seedu/duke/Project.java
@@ -7,6 +7,9 @@ public class Project {
     protected boolean isDone;
     protected String deadline;
     private TaskList tasks;
+    private static final String TICK_MARK = "\u2713";
+    private static final String CROSS_MARK = "\u2718";
+
 
     public Project(String description, String deadline) {
         this.description = description;
@@ -16,12 +19,17 @@ public class Project {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        
+        return (isDone ? TICK_MARK : CROSS_MARK); //return tick or X symbols
+        
     }
 
-    public ArrayList<Task> getTasks() {return tasks.getTaskList();}
+    public ArrayList<Task> getTasks() {
+        return tasks.getTaskList();
+    }
 
     public void createTask(String description, String deadline) {
+        
         tasks.createTask(description, deadline);
     }
 
@@ -32,6 +40,8 @@ public class Project {
      */
     @Override
     public String toString() {
+        
         return description + " | " + deadline;
+        
     }
 }
