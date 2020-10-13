6882c6a7d0b0970d9b9bc674d4bf55bbd1bb8a99
shreytheshreyas <shreyaskumar52@gmail.com>
2020-10-13 10:44:46 +0800

    Implemented the remove memeber functionality

---
+++ src/main/java/seedu/duke/commands/member/DeleteTeamMemberCommand.java
@@ -0,0 +1,22 @@
+package seedu.duke.commands.member;
+
+import seedu.duke.commands.Command;
+import seedu.duke.project.ProjectList;
+import seedu.duke.member.TeamMemberList;
+public class DeleteTeamMemberCommand extends Command{
+    private String name;
+
+    public DeleteTeamMemberCommand(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void executeCommand(ProjectList projects) {
+        TeamMemberList.removeTeamMember(name);
+    }
+
+    @Override
+    public Boolean isExit() {
+        return false;
+    }
+}
