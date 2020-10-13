df34dc165e6f6156a7d00d406266fb362408b2f1
thatseant <46109197+thatseant@users.noreply.github.com>
2020-10-13 16:45:35 +0800

    Extract all System.out.println to Ui methods
    New class Ui handles all output.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,6 +3,7 @@ package seedu.duke;
 import seedu.duke.commands.Command;
 import seedu.duke.project.ProjectList;
 import seedu.duke.task.TaskList;
+import seedu.duke.ui.Ui;
 
 import java.util.Scanner;
 
@@ -14,32 +15,23 @@ public class Duke {
     public static void main(String[] args) throws DukeExceptions {
 
         ProjectList projects = new ProjectList();
+        Ui ui = new Ui();
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
-        System.out.println("Hello from Duke!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-
-
+        ui.printWelcome();
         Scanner in = new Scanner(System.in);
 
         boolean isExit = false;
         while (!isExit) {
             try {
                 Command commandInput = Parser.parse(in.nextLine());
-                System.out.println("____________________________________________________________");
+                ui.printLine();
                 String output = commandInput.executeCommand(projects);
-                System.out.println(output);
+                ui.printOutput(output);
                 isExit = commandInput.isExit();
             } catch (NullPointerException | StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException e) {
                 System.out.println(e);
             }
-            System.out.println("____________________________________________________________");
+            ui.printLine();
         }
 
     }
