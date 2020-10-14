70560256ad5eb5ed8346a16a0dcb5cd3587e6086
thatseant <46109197+thatseant@users.noreply.github.com>
2020-10-14 21:46:19 +0800

    Corrected naming and coding conventions

--- src/test/java/seedu/duke/commands/task/TaskListCommandTest.java
+++ src/test/java/seedu/duke/commands/task/TaskListCommandTest.java
@@ -16,20 +16,15 @@ class TaskListCommandTest {
         ProjectList projects = new ProjectList();
         projects.createProject("iPhone App");
         projects.createProject("Android App");
-        int testIndex = 1;
-        Project iphone = projects.getProjectList().get(testIndex);
-        iphone.createTask("network features", LocalDate.parse("2020-08-24"));
-        iphone.createTask("keyboard features", LocalDate.parse("2020-10-24"));
+        int secondProjectIndex = 1;
+        Project android = projects.getProjectList().get(secondProjectIndex);
+        android.createTask("network features", LocalDate.parse("2020-08-24"));
+        android.createTask("keyboard features", LocalDate.parse("2020-10-24"));
 
         //Expected Output
-        String expectedOutput = "";
-        expectedOutput += "List of Tasks:";
-        expectedOutput += "\n     " + "1" + "." + "network features | 24/08/2020";
-        expectedOutput += "\n     " + "2" + "." + "keyboard features | 24/10/2020";
-
-
-
-
+        String expectedOutput = "List of Tasks:"
+                + "\n     " + "1" + "." + "network features | 24/08/2020"
+                + "\n     " + "2" + "." + "keyboard features | 24/10/2020";
         TaskListCommand taskListCommand = new TaskListCommand(1);
         String actualOutput = taskListCommand.executeCommand(projects);
         assertEquals(expectedOutput, actualOutput);
