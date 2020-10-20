18dbd8cadb32dca51f06fff5755804e22ff49898
samuellleow <samuel_leow@hotmail.com>
2020-10-20 23:52:06 +0800

    Fixed Gradle check

--- src/test/java/seedu/duke/ProjectListTest.java
+++ src/test/java/seedu/duke/ProjectListTest.java
@@ -2,16 +2,13 @@ package seedu.duke;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import seedu.duke.commands.project.*;
-import seedu.duke.commands.task.TaskCommand;
+import seedu.duke.commands.project.ProjectListCommand;
 import seedu.duke.project.Project;
-import seedu.duke.ui.Ui;
-import seedu.duke.project.ProjectList;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class ProjectListTest {
     static ArrayList<Project> projects;
@@ -32,10 +29,10 @@ public class ProjectListTest {
     @Test
     void executeCommand_differentDeadline_listOfSortedProjectsByDeadline() throws DukeExceptions {
         ProjectListCommand command = new ProjectListCommand();
-        String expectedOutput = "List of Projects:\n" +
-                "     1.CS2113 Tutorial (2019-01-01) \n" +
-                "     2.Drink Water (2020-12-31) \n" +
-                "     3.Fire";
+        String expectedOutput = "List of Projects:\n"
+                + "     1.CS2113 Tutorial (2019-01-01) \n"
+                + "     2.Drink Water (2020-12-31) \n"
+                + "     3.Fire";
         String actualOutput = command.executeCommand(projects);
         assertEquals(expectedOutput, actualOutput);
     }
