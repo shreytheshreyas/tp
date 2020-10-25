20b62e07afd1af14993976b7aa0b7bc03d83d444
shreytheshreyas <shreyaskumar52@gmail.com>
2020-10-25 15:04:55 +0800

    made test for feature and fixed styling errors

--- src/test/java/seedu/duke/commands/member/AssignMemberToProjectCommandTest.java
+++ src/test/java/seedu/duke/commands/member/AssignMemberToProjectCommandTest.java
@@ -12,24 +12,30 @@ import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class AssignMemberToProjectCommandTest {
     static ArrayList<Project> projects;
     static ArrayList<TeamMember> teamMembers;
     static Ui ui = new Ui();
 
     @BeforeAll
-    static void createLists() {
+    static void createProjectList() {
         projects = new ArrayList<>();
         teamMembers = new ArrayList<>();
         Project project1 = new Project("Project 1");
         Project project2 = new Project("Project 2");
         Project project3 = new Project("Project 3");
-        TeamMember member1 = new TeamMember("Member 1");
-        TeamMember member2 = new TeamMember("Member 2");
-        TeamMember member3 = new TeamMember("Member 3");
         projects.add(project1);
         projects.add(project2);
         projects.add(project3);
+    }
+
+    @BeforeAll
+    static void createMemberList() {
+        teamMembers = new ArrayList<>();
+        TeamMember member1 = new TeamMember("Member 1");
+        TeamMember member2 = new TeamMember("Member 2");
+        TeamMember member3 = new TeamMember("Member 3");
         teamMembers.add(member1);
         teamMembers.add(member2);
         teamMembers.add(member3);
@@ -42,36 +48,36 @@ public class AssignMemberToProjectCommandTest {
         params.put("p","1");
 
         AssignMemberToProjectCommand assignProject = new AssignMemberToProjectCommand(params,true);
-        String ExpectedOutput = "Member 2 assigned to project 1";
-        String ActualOutput = assignProject.executeCommand(projects,teamMembers);
-        assertEquals(ExpectedOutput,ActualOutput);
+        String expectedOutput = "Member 2 assigned to project 1";
+        String actualOutput = assignProject.executeCommand(projects,teamMembers);
+        assertEquals(expectedOutput,actualOutput);
     }
 
     @Test
-    public void executeCommand_invalidMemberId_assignMemberToProject() throws DukeExceptions{
+    public void executeCommand_invalidMemberId_assignMemberToProject() throws DukeExceptions {
         HashMap<String,String> params = new HashMap<>();
         params.put("m","7");
         params.put("p","2");
 
         AssignMemberToProjectCommand assignProject = new AssignMemberToProjectCommand(params,true);
-        String ExpectedOutput = "The member ID entered does not exist";
-        Throwable ActualOutputException = assertThrows(DukeExceptions.class, () -> {
+        String expectedOutput = "The member ID entered does not exist";
+        Throwable actualOutputException = assertThrows(DukeExceptions.class, () -> {
             assignProject.executeCommand(projects,teamMembers);
         });
-        assertEquals(ExpectedOutput,ActualOutputException.toString());
+        assertEquals(expectedOutput,actualOutputException.toString());
     }
 
     @Test
-    public void executeCommand_invalidProjectId_assignMemberToProject() throws DukeExceptions{
+    public void executeCommand_invalidProjectId_assignMemberToProject() throws DukeExceptions {
         HashMap<String,String> params = new HashMap<>();
         params.put("m","1");
         params.put("p","5");
 
         AssignMemberToProjectCommand assignProject = new AssignMemberToProjectCommand(params,true);
-        String ExpectedOutput = "Project ID does not exist!";
-        Throwable ActualOutputException = assertThrows(DukeExceptions.class, () -> {
+        String expectedOutput = "Project ID does not exist!";
+        Throwable actualOutputException = assertThrows(DukeExceptions.class, () -> {
             assignProject.executeCommand(projects,teamMembers);
         });
-        assertEquals(ExpectedOutput,ActualOutputException.toString());
+        assertEquals(expectedOutput,actualOutputException.toString());
     }
 }
