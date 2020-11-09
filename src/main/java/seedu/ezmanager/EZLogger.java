a89e7ab22f26bb0a5671d90734ee45ecd7fc612f
thatseant <46109197+thatseant@users.noreply.github.com>
2020-11-09 10:50:20 +0800

    Added Logger

--- src/main/java/seedu/ezmanager/EZLogger.java
+++ src/main/java/seedu/ezmanager/EZLogger.java
@@ -1,2 +1,32 @@
-package seedu.duke;public class EZLogger {
+package seedu.ezmanager;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class EZLogger {
+    private static Logger ezLogger = Logger.getLogger("EZLogger");
+    private static FileHandler fh;
+    private static boolean toLog;
+
+    public static void setup() {
+        LogManager.getLogManager().reset();
+        toLog = true;
+
+        try {
+            fh = new FileHandler("./ezlogger.log");
+            fh.setLevel(Level.FINE);
+            fh.setFormatter(new SimpleFormatter());
+            ezLogger.addHandler(fh);
+        } catch (IOException e) {
+            ezLogger.log(Level.SEVERE, "File logger not configured", e);
+            toLog = false;
+        }
+    }
+
+    public static void log(Level level, String message) {
+        if (toLog) {
+            ezLogger.log(level, message);
+        }
+    }
+
 }
